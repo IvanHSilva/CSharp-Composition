7f91e69a1d6b4e33f00e241f8b8b13b80c238a09
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-08-29 17:07:56 -0300

    Classes, ENums and Composition created

---
+++ Composition/Entities/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace Composition.Entities {
+    public class Department {
+        //attributes
+        public string Name { get; set; }
+
+        //constructors
+        public Department() { }
+
+        public Department(string name) {
+            Name = name;
+        }
+    }
+}
---
+++ Composition/Entities/HourContract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Composition.Entities {
+    public class HourContract {
+        //attributes
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        //constructors
+        public HourContract() { }
+
+        public HourContract(DateTime date, double valuePerHour, int hours) {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        //methods
+        public double TotalValue() {
+            return Hours * ValuePerHour;
+        }
+    }
+}
